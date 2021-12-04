6c23ab0b6583b82e64f65a76911d4155ca48ed58
ViniciusArauj <viniciusdearaujoesouza@gmail.com>
2021-12-04 20:53:36 -0300

    Update OrganizeHand.h

--- OrganizeHand.h
+++ OrganizeHand.h
@@ -1,1 +1,13 @@
-void OrganizeHand (std::list <std::pair <char,char>> &Piece)
+void OrganizeHand (std::list <std::pair <char,char>> &Piece){
+    int i, atu, prox, *pedra0, *pedra1, *aux;
+
+    for(i=0;i<7;i++){
+        
+        atu = Piece.front(i).first + Piece.front(i).second;
+        prox = Piece.front(i+1).first + Piece.front(i+1).second;
+        
+        if(atu > prox){
+        // ent a pedra no local i deve ser trocada pela pedra i+1    
+        } 
+    }
+}
