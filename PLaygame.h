a95d4c30a8629f87e00033423f826ac98db24fa1
Enzo Gebauer <enzo.gebauer@hotmail.com>
2021-12-04 14:03:04 -0300

    ha

--- PLaygame.h
+++ PLaygame.h
@@ -1,3 +1,10 @@
 void PlayGame(std::list <std::pair<char,char> > &Piece, std::string Name,std::list <std::pair<char,char> > &Table){
+char lado; // sera o lado da lista onde será adicionada a peça já no mesa de jogo
+int Turnc; // Turnc ira determinar se o Turno avançou
+int choice; // selecionar peca
+std:: list <std::pair<char,char> > aux // lista onde serao guardadas as peças que forem
+                                        // tiradas da mao do jogador e depois adicionadas na mesa de jogo oficial.
+while((Turnc == 0)){
 
+}
 }//oi
---
+++ Skipturn.h
@@ -0,0 +1,5 @@
+void SkipTurn(std::list <std::pair<char,char> > Piece,std::list <std::pair<char,char> > Table){
+    while(!Piece.empty()){
+        if(Piece.front)
+    }
+}
